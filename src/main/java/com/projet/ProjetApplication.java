0f6ce9bf5a0c1120932c9211e6dd0886a4354c47
Laurent <laurentsavivanh@gmail.com>
2021-02-16 14:05:20 -0500

    Création Home page + route (search, account, home)

--- src/main/java/com/projet/ProjetApplication.java
+++ src/main/java/com/projet/ProjetApplication.java
@@ -2,12 +2,26 @@ package com.projet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@Controller
 public class ProjetApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjetApplication.class, args);
     }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/account")
+    public String account() {
+        return "account.html";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/search")
+    public String search() {
+        return "search.html";
+    }
+
 
 }
